b38a3033713d0b90535e8a4f375f1944efc1a7e2
Christof Spies <christof.spies@seips.net>
2011-03-20 12:28:20 +0100

    add download to Rad::Installer::Windows

--- lib/rad/installer/windows.rb
+++ lib/rad/installer/windows.rb
@@ -1,5 +1,12 @@
 module Rad::Installer::Windows
   def self.run
+    url = 'http://arduino.googlecode.com/files/arduino-0022.zip'
+    file = 'C:\opt\arduino-0022.zip'
+    download url, file
+    exit
+    extract file
+    exit
+    download_file
     text = <<-WINDOWS
 To install Arduino on Windows follow the instructions on the Website.
 http://arduino.cc/en/Guide/Windows
@@ -7,7 +14,18 @@ WINDOWS
     puts text
   end
 
-  def self.install
-    #TODO
+  def self.download(from_url, to_file)
+
+    open(from_url, 'r', :read_timeout=>600) {|f|
+      File.open(to_file,"wb") do |file|
+        file.puts f.read
+      end
+    }
+    
   end
+
+  def self.extract(file)
+
+  end
+
 end
