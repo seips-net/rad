4d8049d81df4885d756f4c625fbc0ae93b91723a
Christof Spies <christof.spies@seips.net>
2011-03-31 22:38:33 +0200

    remove double comment

--- lib/rad/installer/windows.rb
+++ lib/rad/installer/windows.rb
@@ -1,7 +1,5 @@
 class Rad::Installer::Windows < Rad::Installer::Base
 
-  #  To install Arduino on Windows follow the instructions on the Website. http://arduino.cc/en/Guide/Windows
-
   def self.run
     arduino_url = 'http://arduino.googlecode.com/files/arduino-0022.zip'
     opt_dir = Pathname.new 'C:/opt'
