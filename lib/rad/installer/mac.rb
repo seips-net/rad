8cbb5d7354e094504ad3a7134f3bbf23a6bab6e7
Christof Spies <christof.spies@seips.net>
2011-03-31 22:51:52 +0200

    DRY rad/installer/mac

--- lib/rad/installer/mac.rb
+++ lib/rad/installer/mac.rb
@@ -1,37 +1,20 @@
 module Rad::Installer::Mac
+
   def self.run
-    text = <<-MACOS
-To install Arduino on MacOS follow the instructions on the Website.
+    arduino_url = 'http://arduino.googlecode.com/files/arduino-0022.dmg'
+    opt_dir = Pathname.new '/Applications'
+    arduino_file = opt_dir + arduino_url.split('/').last
+
+    download arduino_url, arduino_file
+    #TODO how to install on arduino
+    #extract arduino_file, opt_dir
+
+    text = <<-TEXT
+To install Arduino on Mac OS X follow the instructions on the Website.
 http://arduino.cc/en/Guide/MacOSX
-MACOS
+You can find Arduino (not jet extracted) in #{opt_dir}
+TEXT
     puts text
-  end
 
-  def self.install
-    #TODO
-  end
-end
-
-class DarwinInstaller
-  def self.install!
-    puts "Downloading arduino-0015 for Mac from Arduino.cc"
-    File.open("/Applications/arduino-0015.zip", "w") do |file|
-      pbar = nil
-      file << open("http://arduino.googlecode.com/files/arduino-0015-mac.zip",
-      :content_length_proc => lambda {|t|
-        if t && 0 < t
-          pbar = ProgressBar.new(" Progress", t)
-          pbar.file_transfer_mode
-        end
-      },
-      :progress_proc => lambda {|s|
-        pbar.set s if pbar
-      }).read
-      pbar.finish
-    end
-    puts "Unzipping..."
-    `cd /Applications; unzip arduino-0015.zip`
-    `rm /Applications/arduino-0015.zip`
-    puts "installed Arduino here: /Applications/arduino-0015"
   end
 end
