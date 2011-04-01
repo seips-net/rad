b3af691a33f9815adf52e0cdc983b8b0c5a142be
Christof Spies <christof.spies@seips.net>
2011-04-01 12:48:40 +0200

    make sketch_name a class variable

--- lib/rad/build.rb
+++ lib/rad/build.rb
@@ -1,9 +1,9 @@
 class Rad::Build
+  class << self; attr_accessor :sketch_name end
   def self.run
-    # Handle options:
-    options, parser = OptionParser.parse(ARGV)
-    sketch_name = ARGV[0]
-    parser.parse!(["-h"]) unless sketch_name
+    
+    @sketch_name = ARGV[0]
+    raise "Sketchname is missing" unless @sketch_name
 
     vendor_rad
     libraries
@@ -19,37 +19,37 @@ class Rad::Build
   def self.vendor_rad
     # Build vendor/rad:
 
-    FileUtils.mkdir_p "#{sketch_name}/vendor/rad"
+    FileUtils.mkdir_p "#{@sketch_name}/vendor/rad"
     puts "Successfully created your sketch directory."
 
-    FileUtils.cp_r "#{File.dirname(__FILE__)}/../lib/rad/.", "#{sketch_name}/vendor/rad"
-    puts "Installed RAD into #{sketch_name}/vendor/rad"
+    FileUtils.cp_r "#{File.dirname(__FILE__)}/../lib/rad/.", "#{@sketch_name}/vendor/rad"
+    puts "Installed RAD into #{@sketch_name}/vendor/rad"
     puts
   end
   
   def self.libraries
     # Build vendor/libraries:
     
-    FileUtils.mkdir_p "#{sketch_name}/vendor/libraries"
+    FileUtils.mkdir_p "#{@sketch_name}/vendor/libraries"
     puts "Successfully created your libraries directory."
     
     libs = %W{ AF_XPort AFSoftSerial DS1307 FrequencyTimer2 I2CEEPROM LoopTimer OneWire Servo Stepper SWSerLCDpa SWSerLCDsf Wire }
     libs.each do |lib|
       src = RAD_LIB.join('libraries',lib,'.')
-      dest = "#{sketch_name}/vendor/libraries/#{lib}"
+      dest = "#{@sketch_name}/vendor/libraries/#{lib}"
       FileUtils.cp_r src, dest
-      puts "Installed #{lib} into #{sketch_name}/vendor/libraries"
+      puts "Installed #{lib} into #{@sketch_name}/vendor/libraries"
     end
   end
 
   def self.examples
     # Build examples -- used for basic testing
 
-    FileUtils.mkdir_p "#{sketch_name}/vendor/libraries"
+    FileUtils.mkdir_p "#{@sketch_name}/vendor/libraries"
     puts "Successfully created your examples directory."
 
-    FileUtils.cp_r "#{File.dirname(__FILE__)}/../lib/examples/.", "#{sketch_name}/examples"
-    puts "Installed examples into #{sketch_name}/examples"
+    FileUtils.cp_r "#{File.dirname(__FILE__)}/../lib/examples/.", "#{@sketch_name}/examples"
+    puts "Installed examples into #{@sketch_name}/examples"
     puts
   end
   
@@ -58,35 +58,35 @@ class Rad::Build
     
     # FIXME: this should put the tests into the vendor/tests directory instead.
     
-    # FileUtils.mkdir_p "#{sketch_name}/test"
+    # FileUtils.mkdir_p "#{@sketch_name}/test"
     # puts "Successfully created your test directory."
     #
-    # FileUtils.cp_r "#{File.dirname(__FILE__)}/../lib/test/.", "#{sketch_name}/test"
-    # puts "Installed tests into #{sketch_name}/test"
+    # FileUtils.cp_r "#{File.dirname(__FILE__)}/../lib/test/.", "#{@sketch_name}/test"
+    # puts "Installed tests into #{@sketch_name}/test"
     # puts
   end
 
   def self.plugin
     # Build vendor/plugins:
 
-    FileUtils.mkdir_p "#{sketch_name}/vendor/plugins"
+    FileUtils.mkdir_p "#{@sketch_name}/vendor/plugins"
     puts "Successfully created your plugins directory."
 
-    FileUtils.cp_r "#{File.dirname(__FILE__)}/../lib/plugins/.", "#{sketch_name}/vendor/plugins"
-    puts "Installed Default plugins into #{sketch_name}/vendor/plugins"
+    FileUtils.cp_r "#{File.dirname(__FILE__)}/../lib/plugins/.", "#{@sketch_name}/vendor/plugins"
+    puts "Installed Default plugins into #{@sketch_name}/vendor/plugins"
     puts
   end
   
   def self.sketch
     # Add an default sketch directory # needed to run test:compile
 
-    FileUtils.mkdir_p "#{sketch_name}/#{sketch_name}"
+    FileUtils.mkdir_p "#{@sketch_name}/#{@sketch_name}"
     puts "Successfully created your default sketch directory."
 
-    FileUtils.touch "#{sketch_name}/#{sketch_name}.rb"
-    File.open("#{sketch_name}/#{sketch_name}.rb", "w") do |file|
+    FileUtils.touch "#{@sketch_name}/#{@sketch_name}.rb"
+    File.open("#{@sketch_name}/#{@sketch_name}.rb", "w") do |file|
       file << <<-EOS
-    class #{sketch_name.split("_").collect{|c| c.capitalize}.join("")} < ArduinoSketch
+    class #{@sketch_name.split("_").collect{|c| c.capitalize}.join("")} < ArduinoSketch
 
     # looking for hints?  check out the examples directory
     # example sketches can be uploaded to your arduino with
@@ -104,23 +104,23 @@ class Rad::Build
     end
       EOS
     end
-    puts "Added #{sketch_name}/#{sketch_name}.rb"
+    puts "Added #{@sketch_name}/#{@sketch_name}.rb"
   end
 
   def self.rake_file
-    File.open("#{sketch_name}/Rakefile", 'w') do |file|
+    File.open("#{@sketch_name}/Rakefile", 'w') do |file|
       file << <<-EOS
     require 'vendor/rad/init.rb'
       EOS
     end
-    puts "Added #{sketch_name}/Rakefile"
+    puts "Added #{@sketch_name}/Rakefile"
   end
   
   def self.config
-    FileUtils.mkdir_p "#{sketch_name}/config"
-    puts "Added #{sketch_name}/config"
+    FileUtils.mkdir_p "#{@sketch_name}/config"
+    puts "Added #{@sketch_name}/config"
     
-    File.open("#{sketch_name}/config/hardware.yml", 'w') do |file|
+    File.open("#{@sketch_name}/config/hardware.yml", 'w') do |file|
       file << "##############################################################
     # Today's MCU Choices (replace the mcu with your arduino board)
     # atmega8 => Arduino NG or older w/ ATmega8
@@ -137,12 +137,12 @@ class Rad::Build
       "
       file << options["hardware"].to_yaml
     end
-    puts "Added #{sketch_name}/config/hardware.yml"
+    puts "Added #{@sketch_name}/config/hardware.yml"
     
-    File.open("#{sketch_name}/config/software.yml", 'w') do |file|
+    File.open("#{@sketch_name}/config/software.yml", 'w') do |file|
       file << options["software"].to_yaml
     end
-    puts "Added #{sketch_name}/config/software.yml"
+    puts "Added #{@sketch_name}/config/software.yml"
   end
 
   def self.text
