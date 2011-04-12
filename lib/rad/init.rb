c615a7355dfdc524ae283fceb60311b3e30f6a7c
Christof Spies <christof.spies@seips.net>
2011-04-12 19:10:08 +0200

    clean up rad/init

--- lib/rad/init.rb
+++ lib/rad/init.rb
@@ -1,15 +1,12 @@
-RAD_ROOT = "#{File.dirname(__FILE__)}/../.." unless defined?(RAD_ROOT)
+require 'rubygems'
+require 'pathname'
 
-unless defined?(PROJECT_DIR_NAME)
-  a = File.expand_path(File.expand_path("#{RAD_ROOT}")).split("/")
-  PROJECT_DIR_NAME = a[a.length-1]
-end
-
+PROJECT_ROOT = Pathname.new(__FILE__).expand_path.dirname.join('..','..') unless defined?(PROJECT_ROOT)
+PROJECT_DIR_NAME = PROJECT_ROOT.basename unless defined?(PROJECT_DIR_NAME)
 PLUGIN_C_VAR_TYPES = "int|void|unsigned|long|short|uint8_t|static|byte|char\\*|uint8_t"
 
 gem "ruby2c", "~>1.0.0.6"
 
 %w(generators/makefile/makefile.rb rad_processor.rb rad_rewriter.rb rad_type_checker.rb variable_processing.rb arduino_sketch.rb arduino_plugin.rb hardware_library.rb tasks/rad.rb sketch_compiler.rb).each do |path|
-  require File.expand_path("#{RAD_ROOT}/vendor/rad/#{path}")
+  require PROJECT_ROOT.join('vendor','rad',path)
 end
-
