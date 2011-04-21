ace43d5fe884a18ce50ff0711aae03c76b64cea5
Christof Spies <christof.spies@seips.net>
2011-04-22 01:15:38 +0200

    move code from bin to lib

--- lib/rad.rb
+++ lib/rad.rb
@@ -1,3 +1,14 @@
+require 'rubygems'
+require 'pathname'
+require 'optparse'
+require 'yaml'
+require 'fileutils'
+require 'open-uri'
+require 'readline'
+
+RAD_LIB = Pathname.new(__FILE__).expand_path.dirname.join('..','lib')
+
+require RAD_LIB.join('option_parser')
 require RAD_LIB.join('rad','version')
 require RAD_LIB.join('rad','installer')
 require RAD_LIB.join('rad','build')
@@ -7,3 +18,19 @@ module Rad
 end
 
 Rad::Config.run
+
+# home = ENV['HOME'] || ENV['USERPROFILE'] || ENV['HOMEPATH']
+# begin
+#   config = YAML::load open(home + "/.rad")
+# rescue
+#   config = false
+# end
+#
+
+if ARGV[0] == "install"
+  Rad::Installer.run
+elsif ARGV[0] == "test"
+  Rad::Test.run
+else
+  Rad::Build.run
+end
