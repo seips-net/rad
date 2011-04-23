3310a32259ebdb6289faabff761439cd39bf02cc
Christof Spies <christof.spies@seips.net>
2011-04-24 00:17:45 +0200

    replace entries with children

--- lib/rad/make.rb
+++ lib/rad/make.rb
@@ -2,11 +2,8 @@ require 'rake'
 
 class Rad::Make
   def self.rake_load
-    task_dir = RAD_LIB.join('rad','tasks')
-    task_dir.entries.each do |entry|
-      # puts entry.realpath return wrong path #todo verify and write bug report, #todo filter by file extension
-      task_entry = task_dir + entry
-      load task_entry if task_entry.file?
+    RAD_LIB.join('rad','tasks').children.each do |child|
+      load child if child.file? and child.extname == '.rake'
     end
   end
 end
