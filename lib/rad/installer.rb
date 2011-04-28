8eb549f6b2540941b9f3dd1c4f657beffefcbb0c
Christof Spies <christof.spies@seips.net>
2011-04-28 22:17:27 +0200

    remove require use lazy loading

--- lib/rad/installer.rb
+++ lib/rad/installer.rb
@@ -1,18 +1,14 @@
 class Rad::Installer
   def self.run
-    require RAD_LIB.join('rad','installer','base')
 
     case RUBY_PLATFORM
     when /linux/ # Linux
-      require RAD_LIB.join('rad','installer','linux')
       Rad::Installer::Linux.run
 
     when /darwin/ # MacOS
-      require RAD_LIB.join('rad','installer','mac')
       Rad::Installer::Mac.run
 
     when /mingw32/ # Windows
-      require RAD_LIB.join('rad','installer','windows')
       Rad::Installer::Windows.run
 
     else
