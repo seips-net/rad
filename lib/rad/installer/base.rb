1bdf2e905c535113a38a576f7821ddc62a124f8f
Christof Spies <christof.spies@seips.net>
2011-04-30 01:25:25 +0200

    add sha1 check

--- lib/rad/installer/base.rb
+++ lib/rad/installer/base.rb
@@ -1,4 +1,5 @@
 require 'fileutils'
+require 'digest/sha1'
 require 'zip/zip'
 
 class Rad::Installer::Base
@@ -11,6 +12,15 @@ class Rad::Installer::Base
       end
     }
   end
+
+  def self.sha1_check_sum_valid?(file_to_check, sha1_check_sum)
+    Digest::SHA1.file(file_to_check).hexdigest == sha1_check_sum
+  end
+
+# #todo detect file extension and use correct extract function
+#  def self.extract(from_file, to_dir)
+#    
+#  end
 
   # bug in rubyzip hope to be fixed in 0.9.5
   # https://github.com/aussiegeek/rubyzip/pull/1
